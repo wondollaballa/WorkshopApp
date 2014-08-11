4c3dc1b2391c8860593f4846848d93889aa6478e
wondollaballa <onedough83@gmail.com>
2014-08-11 17:52:29 +0900

    First Android App

---
+++ app/src/main/java/com/eyelevate/wondochoung/workshopapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.eyelevate.wondochoung.workshopapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseObject;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, getString(R.string.toast_message), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Treehouse temp = new Treehouse();
+
+        ParseObject object;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/eyelevate/wondochoung/workshopapp/Treehouse.java
@@ -0,0 +1,7 @@
+package com.eyelevate.wondochoung.workshopapp;
+
+/**
+ * Created by wondochoung on 8/11/14.
+ */
+public class Treehouse {
+}
